50d1e1c9754f0a4545c69409ac8e1e97726a843e
walterzhao <519362600@qq.com>
2022-04-03 17:42:02 +0800

    Revert "add cmake first commit"
    This reverts commit e4388cd91cbdce55b7a4edd6031cb2b5237d32e6.

--- 工具/cmake/cmake 学习/1_base/02_include/include/hello.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Hello
-{
-public:
-    void print();
-};
--- 工具/cmake/cmake 学习/1_base/02_include/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "hello.h"
-
-int main(int argc, char *argv[])
-{
-    Hello hi;
-    hi.print();
-    return 0;
-}
